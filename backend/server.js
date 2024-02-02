ba0c3d59e5a5a5fd13277e670b5ca572f4a25797
Abayraj <abayunni5@gmail.com>
2024-02-03 00:39:11 +0530

    changes in server file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import connectDatabase  from'./config/database.js'
 
+
+
 // Convert import.meta.url to a file path
 const __filename = fileURLToPath(import.meta.url);
 // Get the directory name
@@ -12,10 +14,6 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
 connectDatabase();
-
-
-console.log(process.env.NODE_ENV)
-
 app.listen(process.env.PORT, () => {
   console.log(`server started on ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
