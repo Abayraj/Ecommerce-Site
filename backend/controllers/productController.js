8e3e9b203f4581d5279bcb2e1f6d6848c57d64b5
Abayraj <abayunni5@gmail.com>
2024-02-02 13:24:39 +0530

    Add new routes to product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,27 @@
+import Product from "../models/product.js";
 
-export const getProducts = (req,res,next) =>{
+///Create new product =>/api/v1/product/new
+export const  newProduct = async (req,res,next) =>{
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    });
+
+
+} ;
+///Create get products =>/api/v1/product/new
+export const  getProducts = (req,res,next) =>{
     res.status(200).json({
         success:true,
         message:"This route will show all products in database"
+    });
+};
+
+export const deleteProducts =(req,res,next) =>{
+    res.status(204).json({
+        success:true,
+        message:"This route will delete selected product in database"
     });
 };
