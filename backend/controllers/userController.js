1c84debf60d32604630828914316e57e4ee3b31d
Abayraj <abayunni5@gmail.com>
2024-02-16 11:58:04 +0530

    feat:feat:logout functionality and remove token from cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,9 +49,18 @@ export const loginUsers = catchAsyncErrors(async (req, res, next) => {
   sendToken(user,200,res)
 });
 
-export const logOutUsers = catchAsyncErrors(async(req,res,next)=>{
-  const {email} = req.body;
-  if (!email) {
-    return next(new ErrorHandler("please enter email and password", 400));
-  }
+//Logout user /api/v1/logout
+
+export const logOutUser = catchAsyncErrors(async(req,res,next)=>{
+  res.cookie('token',null,{
+    expires: new Date(Date.now()),
+    httpOnly:true
+  })
+
+  res.status(200).json({
+    success:true,
+    message:'Logged out'
+
+  })
+
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import {registerUser,loginUsers} from '../controllers/userController.js';
+import {registerUser,loginUsers,logOutUser} from '../controllers/userController.js';
 
 
 router.post('/register',registerUser)
 router.post('/login',loginUsers)
-router.post('/logout')
+router.get('/logout',logOutUser)
 
 
 export default router
